5eb27e5e0d806bec63b01c20d0b918f53f05dd90
Mikryukov <Mikrykov_91@mail.ru>
2013-11-10 20:00:58 +0400

    simple registration

--- practice_7/library/library/urls.py
+++ practice_7/library/library/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from bookimgs.models import BooksImageList, BooksImageDetail
 from books.models import BooksList, BooksDetail
+from registration import views
 
 admin.autodiscover()
 
@@ -15,4 +16,12 @@ urlpatterns = patterns(
     url(r'^books/list/(?P<page>\d+)/$', BooksList.as_view()),
     url(r'^books/details/(?P<pk>\d+)/$', BooksDetail.as_view(
         template_name="books_detail.html")),
+
+    url(r'^accounts/login/$', views.login),
+    url(r'^accounts/auth/$', views.auth_view),
+    url(r'^accounts/loggedin/$', views.loggedin),
+    url(r'^accounts/invalid_login/$', views.invalid_login),
+    url(r'^accounts/register/$', views.register),
+    url(r'^accounts/register_success/$', views.register_success),
+    url(r'^accounts/register_failed/$', views.register_failed),
 )
